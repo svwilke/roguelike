aa4c21927216786ac5b2dbc127fcbfb7b17f7f2a
Sven Wilke <svwilke@uos.de>
2019-03-11 09:23:26 +0100

    Changed wall auto-tiling to only be affected by visible wall tiles

--- SimpleRoguelike/src/apace/gameplay/map/TileWall.java
+++ SimpleRoguelike/src/apace/gameplay/map/TileWall.java
@@ -5,6 +5,7 @@ import java.awt.Graphics2D;
 import apace.drawing.Palette;
 import apace.lib.Sprites;
 import apace.utils.Direction;
+import apace.utils.Flags;
 import apace.utils.Position;
 
 public class TileWall extends Tile {
@@ -15,7 +16,7 @@ public class TileWall extends Tile {
 	
 	@Override
 	public void render(Graphics2D g, Map map, Palette p, Position pos) {
-		int bitfield = getSurrounding(map, pos);
+		int bitfield = getVisibleSurrounding(map, pos);
 		if(Sprites.WALLS[bitfield] != null) {
 			super.render(g, p, Sprites.WALLS[bitfield], pos);
 		} else {
@@ -25,7 +26,7 @@ public class TileWall extends Tile {
 		//g.drawString("" + bitfield, pos.getPixelX(), pos.getPixelY() + 5);
 		//Render.drawText(g, "" + bitfield, pos.getPixelX(), pos.getPixelY(), Color.white);
 		Position below = pos.down();
-		if(map.isInBounds(below)) {
+		if(map.isInBounds(below) && !Flags.get(bitfield, Flags.DOWN)) {
 			//p.swap(Palette.CLEAR, Palette.BLACK);
 			//System.out.println("tru");
 			super.render(g, p, Sprites.HALFWALL, below);
@@ -40,6 +41,20 @@ public class TileWall extends Tile {
 		for(Direction d : Direction.values()) {
 			int same = 1;
 			if(map.isInBounds(d.from(pos)) && !(map.getTile(d.from(pos)) instanceof TileWall)) {
+				same = 0;
+			}
+			b += same * add;
+			add /= 2;
+		}
+		return b;
+	}
+	
+	public int getVisibleSurrounding(Map map, Position pos) {
+		int b = 0;
+		int add = 128;
+		for(Direction d : Direction.values()) {
+			int same = 1;
+			if(map.isInBounds(d.from(pos)) && (!(map.getTile(d.from(pos)) instanceof TileWall) || !map.isVisible(d.from(pos)))) {
 				same = 0;
 			}
 			b += same * add;
